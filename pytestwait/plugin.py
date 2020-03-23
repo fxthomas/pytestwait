094d4db3ab24ca99c0514a8539aa0db896830be4
François-Xavier Thomas <fxthomas@dxomark.com>
2020-03-23 12:29:22 +0100

    Add command-line option to toggle behavior

--- pytestwait/plugin.py
+++ pytestwait/plugin.py
@@ -3,9 +3,15 @@
 import os
 
 def pytest_cmdline_main(config):
-    capman = config.pluginmanager.getplugin("capturemanager")
-    capman.suspend_global_capture(in_=True)
-    print("Current PID: %d" % os.getpid())
-    print("Press ENTER...")
-    raw_input()
+    if config.getoption("--wait"):
+        capman = config.pluginmanager.getplugin("capturemanager")
+        capman.suspend_global_capture(in_=True)
+        print("Current PID: %d" % os.getpid())
+        print("Press ENTER...")
+        raw_input()
     return None  # Use the default behavior
+
+
+def pytest_addoption(parser):
+    """Called by pytest to prepare the list of available command-line args"""
+    parser.addoption("--wait", action="store_true", help="display pid and wait before starting tests")
